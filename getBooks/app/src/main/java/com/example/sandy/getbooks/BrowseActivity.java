7c97026062c74623aae3a8434f1550b6e0118882
Evelyn <bbyl88@gmail.com>
2018-06-22 15:50:29 +0800

    BookDetails and Browse

--- getBooks/app/src/main/java/com/example/sandy/getbooks/BrowseActivity.java
+++ getBooks/app/src/main/java/com/example/sandy/getbooks/BrowseActivity.java
@@ -1,9 +1,12 @@
 package com.example.sandy.getbooks;
 
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BrowseActivity extends AppCompatActivity {
 
@@ -11,7 +14,21 @@ public class BrowseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browse);
+
+        // Attempt to launch an activity within our own app
+        Button btnBrowse = (Button)findViewById(R.id.btnBrowse);
+        btnBrowse.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+               Intent intent = new Intent(BrowseActivity.this,BookDetailsActivity.class);
+               intent.putExtra("Rick Riordan",  "@+id/editTextAuthor");
+               startActivity(intent);
+            }
+        });
+
     }
+
+
 
     @Override
     public void onBackPressed() {
