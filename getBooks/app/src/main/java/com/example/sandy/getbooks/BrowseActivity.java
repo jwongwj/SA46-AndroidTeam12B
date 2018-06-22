694b86804e9c65eafef394d1f787c9ea68305492
WinAlreadyLor <sandyongsowt@gmail.com>
2018-06-22 14:14:33 +0800

    added search view to browse activity

--- getBooks/app/src/main/java/com/example/sandy/getbooks/BrowseActivity.java
+++ getBooks/app/src/main/java/com/example/sandy/getbooks/BrowseActivity.java
@@ -4,13 +4,39 @@ import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.widget.Toast;
 
 public class BrowseActivity extends AppCompatActivity {
 
+    private SearchView searchView;
+    private String[] data = new String[]{"one","two","three"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browse);
+
+        searchView = (SearchView) findViewById(R.id.searchView);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                if(data[0].contains(query)){
+//                    adapter.getFilter().filter(query);
+                    Toast.makeText(BrowseActivity.this, "Match found", Toast.LENGTH_LONG).show();
+
+                }else{
+                    Toast.makeText(BrowseActivity.this, "No Match found", Toast.LENGTH_LONG).show();
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //    adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
     }
 
     @Override
@@ -28,5 +54,7 @@ public class BrowseActivity extends AppCompatActivity {
                     }
                 }).create().show();
     }
+
+
 
 }
