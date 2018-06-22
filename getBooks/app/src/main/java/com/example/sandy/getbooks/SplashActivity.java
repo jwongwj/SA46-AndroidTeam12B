066c7a50c0c72597be6985bb30b41e27d64e17e9
WinAlreadyLor <sandyongsowt@gmail.com>
2018-06-22 13:54:12 +0800

    added simple splash activity

--- getBooks/app/src/main/java/com/example/sandy/getbooks/SplashActivity.java
+++ getBooks/app/src/main/java/com/example/sandy/getbooks/SplashActivity.java
@@ -1,17 +1,30 @@
 package com.example.sandy.getbooks;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
+
+    /*Duration of wait*/
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, BrowseActivity.class);
-        startActivity(intent);
-        finish();
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the MainActivity. */
+                Intent mainIntent = new Intent(SplashActivity.this, BrowseActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
 }
