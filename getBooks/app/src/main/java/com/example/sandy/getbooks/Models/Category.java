4d83ae92a89c770f2c9cbe9e3e4d16e1a08f784f
WinAlreadyLor <sandyongsowt@gmail.com>
2018-06-24 04:18:15 +0800

    added search criteria by category name to browse activity

--- getBooks/app/src/main/java/com/example/sandy/getbooks/Models/Category.java
+++ getBooks/app/src/main/java/com/example/sandy/getbooks/Models/Category.java
@@ -2,6 +2,7 @@ package com.example.sandy.getbooks.Models;
 
 import com.example.sandy.getbooks.Utils.Configs;
 import com.example.sandy.getbooks.Utils.JSONParser;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -26,6 +27,23 @@ public class Category extends java.util.HashMap<String,String> {
             for (int i=0; i<a.length(); i++) {
                 String c = a.getString(i);
                 list.add(c);
+            }
+        } catch (Exception e) {
+        }
+        return list;
+    }
+
+    public static List<Category> listCategory2() {
+        List<Category> list = new ArrayList<Category>();
+        try {
+            JSONArray a = JSONParser.getJSONArrayFromUrl(Configs.BASE_HOST_URL+"Categories");
+            for (int i=0; i<a.length(); i++) {
+//                Category c =  a.get(i);
+//                list.add(c);
+                JSONObject c = a.getJSONObject(i);
+                Gson gson= new Gson();
+                Category category = gson.fromJson(c.toString(),Category.class);
+                list.add(category);
             }
         } catch (Exception e) {
         }
